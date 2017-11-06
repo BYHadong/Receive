5fde7b4f16e55dc064f039f571196a52272e8c70
Adong BYH <byou0070@gmail.com>
2017-11-07 08:25:06 +0900

    permission check

--- app/src/main/java/org/adong/byh/myreceive/MainActivity.java
+++ app/src/main/java/org/adong/byh/myreceive/MainActivity.java
@@ -1,7 +1,13 @@
 package org.adong.byh.myreceive;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "수신 권한이 있음", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "수신 권한이 없음", Toast.LENGTH_SHORT).show();
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)){
+                Toast.makeText(this, "수신 권한 설명 필요", Toast.LENGTH_SHORT).show();
+            } else {
+                ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.RECEIVE_SMS}, 1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0){
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(this, "SMS권한을 사용자가 승인함", Toast.LENGTH_SHORT).show();
+                    } else if (grantResults[0] == PackageManager.PERMISSION_DENIED){
+                        Toast.makeText(this, "SMS권한을 사용자가 거부함", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "SMS권한을 부여받지 못함", Toast.LENGTH_SHORT).show();
+                }
+        }
     }
 }
