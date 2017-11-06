576d22acdd07170892ad688eaad4260d174e2915
Adong BYH <byou0070@gmail.com>
2017-11-06 14:04:34 +0900

    update

--- app/src/main/java/org/adong/byh/myreceive/MyReceiver.java
+++ app/src/main/java/org/adong/byh/myreceive/MyReceiver.java
@@ -8,10 +8,12 @@ import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MyReceiver extends BroadcastReceiver {
     private static final String TAG = "SMSreceive";
+    private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive 호출");
@@ -27,12 +29,28 @@ public class MyReceiver extends BroadcastReceiver {
 
             Date receivedDate = new Date(messages[0].getTimestampMillis());
             Log.d(TAG,"received date : " + receivedDate);
+            sendToActivity(context,sender, contents, receivedDate);
         }
+    }
+
+    private void sendToActivity(Context context, String sender, String contents, Date receivedDate){
+        Intent intent = new Intent(context, Main2Activity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|
+                        Intent.FLAG_ACTIVITY_NEW_TASK|
+                        Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.putExtra("sender", sender);
+        intent.putExtra("contents", contents);
+        intent.putExtra("receivedDate", format.format(receivedDate));
+
+        context.startActivity(intent);
     }
 
     private SmsMessage[] parseSmsMessage(Bundle bundle){
         Object[] obj = (Object[]) bundle.get("pdus");
-        SmsMessage[] messages = new SmsMessage[obj.length];
+        SmsMessage[] messages = new SmsMessage[0];
+        if (obj != null) {
+            messages = new SmsMessage[obj.length];
+        }
 
         for (int i = 0; i<obj.length; i++){
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
